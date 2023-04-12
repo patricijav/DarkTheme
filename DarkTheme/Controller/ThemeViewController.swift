d0a69506559f2eeb9ffb8a99c14f31709ae757ef
Patrīcija Vainovska <vainovska.patricija@gmail.com>
2023-04-12 14:44:22 +0300

    Finished the homework

--- DarkTheme/Controller/ThemeViewController.swift
+++ DarkTheme/Controller/ThemeViewController.swift
@@ -10,30 +10,66 @@ import UIKit
 class ThemeViewController: UIViewController {
 
     @IBOutlet weak var darkThemeButton: UIButton!
-    var mainText: String = "Dark Theme on"
     var darkIsOn: Bool = false
+    // Marking these as optional, so the starting state can be changed from one place
+    var mainText: String?
+    var backgroundColor: UIColor?
+    var textColor: UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad from ViewController")
+        
+        // Setting this once
         darkThemeButton.layer.cornerRadius = 8
-        // Do any additional setup after loading the view.
+        
+        // Initialize starting state based on darkIsOn variable
+        mainText = getDarkModeStateText(isOn: darkIsOn)
+        backgroundColor = darkIsOn ? UIColor.black : UIColor.white
+        textColor = darkIsOn ? UIColor.white : UIColor.black
+        
+        // Set the initial view
+        updateTheView()
     }
 
     @IBAction func darkThemeButtonTapped(_ sender: Any) {
-        darkThemeIsOn(isOn: darkIsOn)
+        print("darkThemeButtonTapped")
+        
+        flipDarkTheme(isOn: darkIsOn)
     }
     
-    func darkThemeIsOn(isOn: Bool) {
-        //mainText= "Dark Theme On"
-        print("darkThemeButtonTapped")
-        darkThemeButton.setTitle(mainText, for: .normal)
-        darkThemeButton.setTitleColor(UIColor.black, for: .normal)
+    func flipDarkTheme(isOn: Bool) {
+        // Changing the parameters
+        darkIsOn = !darkIsOn
+        mainText = getDarkModeStateText(isOn: darkIsOn)
+        textColor = invertColor(theColor: textColor!)
+        backgroundColor = invertColor(theColor: backgroundColor!)
         
+        // Update the screen
+        updateTheView()
+    }
+    
+    func updateTheView() {
+        // Activating the parameters
+        darkThemeButton.setTitle(mainText, for: .normal)
+        darkThemeButton.setTitleColor(textColor, for: .normal)
         navigationItem.title = mainText
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key
-            .foregroundColor: UIColor.red]
-        view.backgroundColor = UIColor.black
+            .foregroundColor: textColor!]
+        view.backgroundColor = backgroundColor
+    }
+    
+    // Works only with Black and White colors
+    func invertColor(theColor: UIColor) -> UIColor {
+        if theColor == UIColor.white {
+            return UIColor.black
+        } else {
+            return UIColor.white
+        }
+    }
+    
+    func getDarkModeStateText(isOn: Bool) -> String {
+        return "Dark theme is \(isOn ? "on" : "off")"
     }
 }
 
